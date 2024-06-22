99f29fd795ce2066a07c6444dec62efdb057c691
makadenouk <makaden.oukk@gmail.com>
2024-06-22 19:07:04 +0700

    request helper and create project schema

---
+++ src/utils/request-helper.ts
@@ -0,0 +1,38 @@
+import { Schema } from "joi";
+import { Context } from "koa";
+
+export class RequestHelper {
+  context: Context;
+
+  constructor(context: Context) {
+    this.context = context;
+  }
+
+  public getBody(schema: Schema): any {
+    const requestBody = this.context.request.body;
+
+    const { error } = schema.validate(requestBody);
+    if (error) {
+      this.context.throw(
+        400,
+        `Validation error: ${error.details.map((x) => x.message).join(", ")}`
+      );
+    }
+
+    return requestBody;
+  }
+
+  public getParam(field: string): string {
+    const param: string = this.context.params[field];
+    return param;
+  }
+
+  public getCurrentUser(): any {
+    return this.context.state.user || null;
+  }
+
+  public getQuery(field: string): string | null {
+    const query: string | undefined = this.context.query[field];
+    return query || null;
+  }
+}
